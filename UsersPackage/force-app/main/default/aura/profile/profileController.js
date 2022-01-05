f7466fd1eec4af82bbfd25f538dfa2710f168902
Alan Huang <alanghuang@gmail.com>
2022-01-05 10:43:41 -0500

    profile scaffolding

--- UsersPackage/force-app/main/default/aura/profile/profileController.js
+++ UsersPackage/force-app/main/default/aura/profile/profileController.js
@@ -1,5 +1,16 @@
 ({
-    myAction : function(component, event, helper) {
+    init : function(component, event, helper) {
+		component.set('v.updatedCount', 12)
+        component.set('v.selectedItem', 'reports_recent');
+        component.set('v.currentContent', 'reports_recent');
+    },
+    
+    handleSelect: function(component, event, helper) {
+        var selected = event.getParam('name');
 
+        if (selected === 'reports_updated') {
+            component.set('v.updatedCount', 0);
+        }
+        component.set('v.currentContent', selected);
     }
 })
