61c90c69a9f9f47d9c4c89821d48602bcc9a1ddf
Alan Huang <alanghuang@gmail.com>
2022-01-06 12:55:03 -0500

    Users-Package Release 1

--- UsersPackage/force-app/main/default/aura/profile/profileController.js
+++ UsersPackage/force-app/main/default/aura/profile/profileController.js
@@ -1,8 +1,42 @@
 ({
     init : function(component, event, helper) {
-		component.set('v.updatedCount', 12)
+        console.log('init');
+		component.set('v.updatedCount', 12);
         component.set('v.selectedItem', 'reports_recent');
         component.set('v.currentContent', 'reports_recent');
+        
+        var loadContact = component.get("c.getProfile");
+        loadContact.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var profile = response.getReturnValue();
+                component.set("v.currentProfile", profile);
+                console.log(response.getReturnValue());
+            } else if (state === "ERROR") {
+                for (const e of response.getError()) {
+                    console.error(e.message);
+                }
+            }
+        });
+        
+        $A.enqueueAction(loadContact);
+        
+        var loadBids = component.get("c.getBids");
+        loadBids.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var bids = response.getReturnValue();
+                component.set("v.bids", bids);
+                console.log('bids');
+                console.log(response.getReturnValue());
+            } else if (state === "ERROR") {
+                for (const e of response.getError()) {
+                    console.error(e.message);
+                }
+            }
+        });
+        $A.enqueueAction(loadBids);
+        
     },
     
     handleSelect: function(component, event, helper) {
