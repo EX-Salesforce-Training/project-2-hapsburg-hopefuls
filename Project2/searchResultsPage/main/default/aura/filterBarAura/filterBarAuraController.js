91d9a383d02ba9dbf7a1ab5807be448327a9d130
Matthew Lewandowski <mblewandowski96@gmail.com>
2022-01-06 18:31:53 -0500

    searchBar touch up

--- Project2/searchResultsPage/main/default/aura/filterBarAura/filterBarAuraController.js
+++ Project2/searchResultsPage/main/default/aura/filterBarAura/filterBarAuraController.js
@@ -1,7 +1,13 @@
 ({
-    handleFilter : function(component, event, helper) {
+    handleFilter: function (component, event, helper) {
         var data = component.get("v.value");
         const message = { filterData: data };
         component.find("Filter").publish(message);
+    },
+    handleTag: function (component, event, helper) {
+        var data = event.target.value;
+        const message = { filterData: data };
+        sessionStorage.setItem("SearchResult", "potato");
+        component.find("Filter").publish(message);
     }
-})
+});
--- Project2/searchResultsPage/main/default/lwc/searchResultsCard/searchResultsCard.js
+++ Project2/searchResultsPage/main/default/lwc/searchResultsCard/searchResultsCard.js
@@ -1,47 +1,54 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api, track } from "lwc";
 import {
     subscribe,
     unsubscribe,
     APPLICATION_SCOPE,
     MessageContext,
     createMessageContext
-} from 'lightning/messageService';
-import getSearchResults from '@salesforce/apex/searchResultsPageController.getSearchResults'
-import FILTER from '@salesforce/messageChannel/Filter__c';
+} from "lightning/messageService";
+import getSearchResults from "@salesforce/apex/searchResultsPageController.getSearchResults";
+import FILTER from "@salesforce/messageChannel/Filter__c";
 
 export default class SearchResultsCard extends LightningElement {
-
     context = createMessageContext();
     subscription = null;
     @track filterValue;
     @track displayCards = [];
+    @track test;
 
     connectedCallback() {
+        this.test = sessionStorage.getItem("SearchResult");
         let results = getSearchResults();
-        results.then(res => {
-            for(let i = 0; i < Object.keys(res).length; i++) {
-                const newObj = Object.assign({display: true}, res[i]);
+        results.then((res) => {
+            for (let i = 0; i < Object.keys(res).length; i++) {
+                const newObj = Object.assign({ display: true }, res[i]);
                 this.displayCards.push(newObj);
             }
-        })
+            console.log(this.displayCards);
+        });
         this.subscribeMC();
     }
 
     subscribeMC() {
-        if(this.subscription) {
+        if (this.subscription) {
             return;
         } else {
-            this.subscription = subscribe(this.context, FILTER, (message) => {
-                this.handleMessage(message)
-            }, { scope: APPLICATION_SCOPE });
+            this.subscription = subscribe(
+                this.context,
+                FILTER,
+                (message) => {
+                    this.handleMessage(message);
+                },
+                { scope: APPLICATION_SCOPE }
+            );
         }
     }
 
     handleMessage(message) {
         this.filterValue = message.filterData;
         console.log(this.filterValue);
-        for(let card of this.displayCards) {
-            if(card.Product_Tags__c.includes(this.filterValue)) {
+        for (let card of this.displayCards) {
+            if (card.Product_Tags__c.includes(this.filterValue)) {
                 card.display = true;
             } else {
                 card.display = false;
