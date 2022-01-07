c8bd2f8b64fcb8e3f64e72245b184c2669a66e3d
Matthew Lewandowski <mblewandowski96@gmail.com>
2022-01-06 20:04:00 -0500

    testing search

--- Project2/headerBar/main/default/lwc/navBar/navBar.js
+++ Project2/headerBar/main/default/lwc/navBar/navBar.js
@@ -2,4 +2,8 @@ import { LightningElement } from "lwc";
 
 export default class NavBar extends LightningElement {
     loggedin = true;
+
+    handleSearch(event) {
+        window.location.href = "/search-result";
+    }
 }
